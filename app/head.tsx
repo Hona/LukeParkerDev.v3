2a9efe2aa58af34f90798413893fb32ba453e70f
Luke Parker <10430890+Hona@users.noreply.github.com>
2024-12-18 14:24:32 +1000

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -14,13 +14,6 @@ export default function Head() {
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <script type="text/javascript">
-      (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "pfq3nookbp");
-      </script>
     </>
   )
 }
