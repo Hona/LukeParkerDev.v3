c08cdf3b7a28c36d50b295199e4254098fab7949
Luke Parker <10430890+Hona@users.noreply.github.com>
2024-12-18 14:25:19 +1000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -120,6 +120,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           src="https://static.cloudflareinsights.com/beacon.min.js"
           data-cf-beacon='{"token": "8d3a1414c3204cfbb105eac703f01690"}'
         ></script>
+        <Script 
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+        dangerouslyIdentifier
+      >
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "pfq3nookbp");
+        `}
+      </Script>
         {/* End Cloudflare Web Analytics */}
       </body>
     </html>
