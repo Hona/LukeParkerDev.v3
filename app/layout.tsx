e026f687b697ad5febd6b23b6dbc1a5253cabf31
Luke Parker <10430890+Hona@users.noreply.github.com>
2024-12-18 15:21:58 +1000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -122,18 +122,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           data-cf-beacon='{"token": "8d3a1414c3204cfbb105eac703f01690"}'
         ></script>
         <Script 
-  id="microsoft-clarity"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-      (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "pfq3nookbp");
-    `
-  }}
-/>
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "pfq3nookbp");
+            `,
+          }}
+        />
         {/* End Cloudflare Web Analytics */}
       </body>
     </html>
