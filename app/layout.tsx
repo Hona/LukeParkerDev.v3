c5a9b68868df1748e6281e0f239861336628b633
Luke Parker <10430890+Hona@users.noreply.github.com>
2024-12-20 10:03:19 +1000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -124,16 +124,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Script
           id="microsoft-clarity"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
+        >
+          {`
               (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
               })(window, document, "clarity", "script", "pfq3nookbp");
-            `,
-          }}
-        />
+          `}
+        </Script>
         {/* End Cloudflare Web Analytics */}
       </body>
     </html>
